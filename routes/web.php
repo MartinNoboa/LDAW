37beed1aba79cfa6874cc206b67d823161604611
MartinNoboa <nomonoboa@gmail.com>
2021-04-13 21:20:30 -0500

    Add bootstrap to project

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/landing', function () {
     return view('landing');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
