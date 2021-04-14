84b4f3155db04fd00dea8f34d90a45f6bab1286f
MartinNoboa <nomonoboa@gmail.com>
2021-04-13 21:06:25 -0500

    Add views and css files

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- CSS -->
+       <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" >
+
+    </head>
+    <body>
+        <div class = "container">
+            <h2>Miembros del equipo</h2>
+        </div>
+        
+    </body>
+</html>
+
