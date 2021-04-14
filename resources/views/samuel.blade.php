bebd1b634011681068bdf54be86c6045bde587e8
Samuel Gonzalez <A01704696@itesm.mx>
2021-04-14 12:05:29 -0500

    plantilla y vista samuel

---
+++ resources/views/samuel.blade.php
@@ -0,0 +1,19 @@
+
+
+
+@section('seccion')
+
+    <div class="container">
+        <h3>Samuel Octavio Gonzalez Azpeitia</h3>
+        <h3>A01704696</h3>
+        <img src="" alt="">
+        <p>
+            Tengo un año de experiencia en desarrollo web con el proyecto del
+            semestre pasado de bloque 1 y ahora con bloque 2.
+            Lenguages y frameworks que conozco: HTML, Javascript, CSS, PHP, Bootstrap
+            y ahora en proceso de aprender Laravel
+        </p>
+
+    </div>
+
+@endsection
