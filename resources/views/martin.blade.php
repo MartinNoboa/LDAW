0de4c2ab81436cd0016da38d9c4048415a031c86
MartinNoboa <nomonoboa@gmail.com>
2021-04-14 19:05:09 -0500

    Agregar CSS pag martin

--- resources/views/martin.blade.php
+++ resources/views/martin.blade.php
@@ -4,16 +4,29 @@
 @section('seccion')
 
     <div class="container">
+        <div class="row justify-content-center">
         
-        <h3>Martin Noboa</h3>
-        <h3>A01704052</h3>
-        <img src="{{ url('img/martin.jpeg') }}" alt="" style="width:300px; height:350px">
-        <p>
+            <h3 class="row justify-content-center">Martin Noboa</h3>
+            <h3 class="row justify-content-center">A01704052</h3>
+            <img src="{{ url('img/martin.jpeg') }}" alt="" style="width:300px; height:350px">
             
-        </p>
-        
-        <a href = "{{ route ('atras') }}" class = "btn btn-primary">Atras</a>
 
+        </div>
     </div>
+        <div class = "container">
+        
+        
+        <div class="row justify-content-center">
+           <div class="col-sm">
+            </div>
+            <div class="col-sm">
+                <a href = "{{ route ('atras') }}" role = "button" class = "btn btn-outline-dark" >Atras</a>
+            </div>
+            <div class="col-sm">
+            </div>
+        </div>
+
+        </div>
+    
 
 @endsection
