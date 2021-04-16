28d9a16ea67d8014a123deb40962163f0bc463ca
MartinNoboa <nomonoboa@gmail.com>
2021-04-15 19:25:31 -0500

    Agregar CSS y contenido a martin.blade

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,16 +4,18 @@
 
 
 <!-------------------------------------------- Falta agregar css para landing ------------------------------------------>
-    <div class = "container">
-        <h2>Miembros del equipo 7 </h2>
-        <table>
-            <tbody>
-               <a href = "{{ route ('samuel') }}" class = "btn btn-primary">Samuel Gonzalez</a>
-               &nbsp
-               <a href = "{{ route ('martin') }}" class = "btn btn-primary">Martin Noboa</a>
-               &nbsp
-            </tbody> 
-        </table>
+    <div id = "body" class = "container">
+        <div class="row justify-content-center ">
+            <h2 id = "titulo" class = "row justify-content-center">Miembros del equipo 7 </h2>
+            <table>
+                <tbody>
+                   <a href = "{{ route ('samuel') }}" class = "btn btn-outline-dark" type= "button">Samuel Gonzalez</a>
+                   &nbsp
+                   <a href = "{{ route ('martin') }}" class = "btn btn-outline-dark" type= "button">Martin Noboa</a>
+                   &nbsp
+                </tbody> 
+            </table>
+        </div>
     </div>
 
 @endsection
--- resources/views/martin.blade.php
+++ resources/views/martin.blade.php
@@ -3,14 +3,24 @@
 
 @section('seccion')
 
-    <div class="container-fluid">
+    <div id = "bodyMartin" class="container-fluid">
         <div class="row justify-content-center">
         
             <h3 class="row justify-content-center">Martin Noboa</h3>
             <h3 class="row justify-content-center">A01704052</h3>
             <img src="{{ url('img/martin.jpeg') }}" alt="" style="width:300px; height:350px">
             
-
+            <h4>Hobbies</h4>
+            <ul>
+                <li>Me gusta mucho leer, de todo un poco. El libro que me estoy leyendo ahora es "Existencialismo es un humanismo" de Sartre, 10/10 recomendado.</li>
+                <li>Aparte de eso, cuando no estabamos en pandemia, disfrutaba salir con mis amigos.</li>
+            </ul>
+            <h4>Experiencia</h4>
+            <ul>
+                <li>Mi experiencia en desarrollo web viene de Bloque 1 y de mis proyectos personales.</li>
+                <li>Por el momento, estoy cómodo desarrollando en HTML, CSS,PHP y Javascript. Los frameworks que se manejar son UIKit y un poco de Bootstrap</li>
+                <li>En el futuro planeo trabajar con React, Vue Js y Angular, esto en proyectos personales solo para conocer los frameworks. Ya si me siento aventurero,usare Django para un proyecto personal igual.</li>
+            </ul>
         </div>
   
         
